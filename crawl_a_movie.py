daf77fbf7d6af6c3e77e49631c362e16ee8c0b75
lucas-ubuntu-laptop <lucas.weisr@gmail.com>
2013-11-03 01:37:48 +0800

    add single web crawler

---
+++ crawl_a_movie.py
@@ -0,0 +1,27 @@
+import urllib
+import sys
+import json
+
+from BeautifulSoup import BeautifulSoup as Soup
+import BeautifulSoup
+from soupselect.soupselect import select
+
+def craw_a_movie(url):
+    fd = urllib.urlopen(url)
+    soup = Soup(fd)
+    data = {}
+    raw = select(soup, '.dta')
+    data['open'] = raw[0].string
+    data['class'] = raw[1].string
+    data['long'] = raw[2].string
+    data['director'] = raw[3].string
+    data['cast'] = raw[4].string
+    for tmp in select(soup, '.border img')[0].attrs:
+        if tmp[0] == 'src':
+            data['cover'] = str(tmp[1])
+            break
+    return data
+
+if __name__ == '__main__':
+    url = "movie.html"
+    print craw_a_movie(url)
