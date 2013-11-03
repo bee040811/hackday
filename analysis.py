75bd3cae2f2d589443ab8c8c4a99cbff50f78bd3
bee040811 <bee040811@gmail.com>
2013-11-03 09:23:27 +0800

    new function to let main easy

--- analysis.py
+++ analysis.py
@@ -19,6 +19,30 @@ from movie_link_parser import getmovielist
 """ crawl movie description"""
 from crawl_a_movie import craw_a_movie
 
+def convertToSecond(input):
+    """ convert to second """
+    pat = re.compile("([0-9]*).([0-9]*)." )
+    duration = re.findall(pat, input )
+    long = 0
+    try :
+        clock = int(duration[0][0])
+        min = int(duration[0][1])
+        long = clock * 60 * 60 + min * 60  
+    except:
+        long = 1 * 60 * 60 + 50 * 60  
+        print duration
+    return long    
+
+def filterTen(term):
+    """ filter sort 10 order"""
+    count = 0
+    dict = {}
+    for k, v in sorted(term.items(), key=lambda kv: kv[1], reverse=True):
+        dict[k] = v
+        count += 1
+        if count >= 10 :
+            break
+    return dict
 
 def analysis():
     con = mdb.connect('localhost', 'root', '1234', 'hackday',charset='utf8') 
@@ -31,12 +55,16 @@ def analysis():
     movie_list= getmovielist()
     for movie in movie_list:
         movie_name = movie
+        # movie init information
         if (movie_name not in movie_words) :
             movie_words[movie_name] = {}
             movie_words[movie_name]['term'] = {}
             movie_words[movie_name]['good'] = 0
             movie_words[movie_name]['bad'] = 0
             movie_words[movie_name]['url'] = movie_list[movie_name]
+            pat = re.compile("id=([0-9]*)" )
+            movie_id= re.findall(pat,movie_list[movie_name]  )
+            movie_words[movie_name]['movie_id'] = movie_id[0]
         # search movie name    
         cur.execute(select('ptt_movie',{"title like": "%"+ movie_name + "%"}))
         for ptt in cur.fetchall():
@@ -54,33 +82,19 @@ def analysis():
         movie_detail = craw_a_movie(movie_words[movie_name]['url'])
         for (type,value) in movie_detail.iteritems():
             movie_words[movie_name][type] = value
+    """
     file = open('movie.txt',"w")
     file.write(json.dumps(movie_words))
-    """
     file = open('movie.txt',"r")
     movie_words= json.loads(file.read())
     """
-    
     for movie in movie_words:
         movie_type = movie_words[movie]
         #tmp = sorted(movie_type['term'].items(), key=itemgetter(1),reverse=True)
-        pat = re.compile("([0-9]*).([0-9]*)." )
-        duration = re.findall(pat,movie_type['long']  )
-        long = 0
-        try :
-            clock = int(duration[0][0])
-            min = int(duration[0][1])
-            long = clock * 60 * 60 + min * 60  
-            print long
-        except:
-            print duration
-        count = 0
-        dict = {}
-        for k, v in sorted(movie_type['term'].items(), key=lambda kv: kv[1], reverse=True):
-            dict[k] = v
-            count += 1
-            if count >= 10 :
-                break
-        cur.execute(insert('movie_source',{ "movie_name":movie.decode("utf-8"),"movie_description":movie_type['description'].decode("utf-8"),"movie_url":movie_type['url'],"movie_duration":long,"movie_pic":movie_type['cover'],"word_json":json.dumps(dict),"good":movie_type["good"],"bad":movie_type['bad'],"cast":movie_type["cast"],"director":movie_type["director"]})) 
+        long = convertToSecond(movie_type['long'])
+        dict = filterTen(movie_type['term'])    
+
+        cur.execute(insert('movie_source',{ "movie_name":movie.decode("utf-8"),"movie_description":movie_type['description'].decode("utf-8"),"movie_url":movie_type['url'],"movie_duration":long,"movie_pic":movie_type['cover'],"word_json":json.dumps(dict),"good":movie_type["good"],"bad":movie_type['bad'],"cast":movie_type["cast"],"director":movie_type["director"],"movie_id":movie_type["movie_id"]})) 
+
 if __name__ == '__main__':         
     analysis()
